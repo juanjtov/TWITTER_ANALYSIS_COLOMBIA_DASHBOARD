e03e92dc8c94d4d8d08b333ab6cf1931a4036917
juanjtov <juanjosetmontana@gmail.com>
2021-05-23 00:13:18 -0700

    Delete cloud handler

--- TWEETS_STREAMER/streamer_executor.py
+++ TWEETS_STREAMER/streamer_executor.py
@@ -45,15 +45,15 @@ formatter = logging.Formatter(
 ch.setFormatter(formatter)
 root_logger.addHandler(ch)
 
-cloud_handler = CloudLoggingHandler(on_gce=True, logname="task_runner")
-root_logger.addHandler(cloud_handler)
+# cloud_handler = CloudLoggingHandler(on_gce=True, logname="task_runner")
+# root_logger.addHandler(cloud_handler)
 
 # create the executor that watches the topic, and will run the job task
 test_executor = Executor(topic=TOPIC, project=PROJECT, task_cmd=sample_task, subname='sample_task_task')
 
 # add a cloud logging handler and stderr logging handler
-job_cloud_handler = CloudLoggingHandler(on_gce=True, logname=test_executor.subname)
-test_executor.job_log.addHandler(job_cloud_handler)
+# job_cloud_handler = CloudLoggingHandler(on_gce=True, logname=test_executor.subname)
+# test_executor.job_log.addHandler(job_cloud_handler)
 test_executor.job_log.addHandler(ch)
 test_executor.job_log.setLevel(logging.DEBUG)
 

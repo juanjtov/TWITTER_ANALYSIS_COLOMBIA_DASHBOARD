2142dc93b6460b4e2608124d44545776dadeb1de
juanjtov <juanjosetmontana@gmail.com>
2021-05-27 21:24:17 -0700

    Deploying streamer from a cloud function

---
+++ TWEETS_STREAMER/run_streamer.py
@@ -0,0 +1,72 @@
+import tweepy
+import time
+import re
+
+
+from config import Credentials, Settings
+
+#Authentication
+auth  = tweepy.OAuthHandler(Credentials.API_KEY, \
+                            Credentials.API_SECRET_KEY)
+auth.set_access_token(Credentials.ACCESS_TOKEN,  \
+                      Credentials.ACCESS_TOKEN_SECRET)
+
+#CREATE THE API OBJECT
+api = tweepy.API(auth)
+
+#Creat the tweepy Stream Listener
+# Streaming With Tweepy 
+# Override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        
+       
+          # if status.retweeted:
+        # # Avoid retweeted info, and only original tweets will 
+        # # be received
+        #     return True
+
+        # Clean The text
+        text = deEmojify(status.text)
+        cleaned_text = clean_tweet(text)
+        print(cleaned_text)
+        
+             
+
+    def on_error(self, status_code):
+        '''
+        Since Twitter API has rate limits, 
+        stop scraping data as it exceed to the thresold.
+        '''
+        if status_code == 420:
+            # return False to disconnect the stream
+            return False
+
+
+#preprocessing text
+def deEmojify(text):
+    '''
+    Strip all non-ASCII characters to remove emoji characters
+    '''
+    if text:
+        return text.encode('ascii', 'ignore').decode('ascii')
+    else:
+        return None
+
+def clean_tweet(tweet): 
+    ''' 
+    Use sumple regex statemnents to clean tweet text by removing links and special characters
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) \
+                                |(\w+:\/\/\S+)", " ", tweet).split()) 
+
+
+
+#CREATE THE STREAM LISTENER
+runtime = 1 #Stream for 60 minutes
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
+myStream.filter(languages=['es'], track = Settings.TRACK_WORDS, is_async=True)
+time.sleep(runtime*60)
+myStream.disconnect()
